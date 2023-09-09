d25cca98e810be841ab9be9584792eb2b1f36771
Kalanzi Peter <kalanzipeter7@gmail.com>
2023-09-09 12:02:32 +0300

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-
-int main(void) /* This is the entry point of our C program */
-{puts("\"Programming is like building a multilingual puzzle\n");
-return (0);
+/**
+ * main- Entry point;returns 0
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
