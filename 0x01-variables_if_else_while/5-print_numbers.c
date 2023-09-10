af5564c0122ac5a6736b820ba882d88d543d7c91
Kalanzi Peter <kalanzipeter7@gmail.com>
2023-09-10 19:20:34 +0300

    Display base 10 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Return: always 0(success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	printf("%d\n", x);
+
+	return (0);
+}
