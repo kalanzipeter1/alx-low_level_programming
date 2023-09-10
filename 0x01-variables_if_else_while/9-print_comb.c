f75f113098bd85e7c070cffee402de4b7930dbb5
Kalanzi Peter <kalanzipeter7@gmail.com>
2023-09-10 21:09:09 +0300

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Return: always 0(success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 48; x < 58; x++)
+	{
+		putchar(x);
+		if (x != 57)
+		{
+			putchar(',');
+		}
+	}
+	putchar('\n');
+	return (0);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
