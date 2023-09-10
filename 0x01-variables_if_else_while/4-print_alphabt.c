d82c81a88114fd39d1936a87f73c995c069d994a
Kalanzi Peter <kalanzipeter7@gmail.com>
2023-09-10 18:35:46 +0300

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Return: always 0 (success)
+*/
+
+int main(void)
+{
+	int x;
+
+	for (x = 97; x < 123; x++)
+	{
+		if (x != 101 && x != 113)
+		{
+			putchar(x);
+
+		}
+	}	
+		putchar('\n');
+		return (0);
+}
