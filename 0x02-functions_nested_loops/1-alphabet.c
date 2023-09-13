1fc1d7a0ceeb2978a418f78b01aa74737d14759a
Kalanzi Peter <kalanzipeter7@gmail.com>
2023-09-13 20:55:26 +0300

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet- prints the alphabet in lower case
+ *
+ *Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+
+_putchar('\n');
+}
+
